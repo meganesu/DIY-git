dc70c64dad9bd6a78dcbc0f26ad0be8497a58624
Megan Sullivan <meganesulli@gmail.com>
2019-01-10 19:27:35 -1000

    [Megan][#14] Sets up webpack source maps

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './app.js',
+  devtool: 'inline-source-map',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
