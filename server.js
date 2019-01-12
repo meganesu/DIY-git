61b7b70bce9295864a11da1a2bfc07d0491c63b5
Megan Sullivan <meganesulli@gmail.com>
2019-01-11 23:02:53 -1000

    [Megan][#13] Adds webpack-dev-middleware for automatic compiling on code changes

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 /* eslint-disable no-console */
 const express = require('express');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
 
 const app = express();
 const port = 3000;
+
+const config = require('./webpack.config.js');
+const compiler = webpack(config);
+
+app.use(webpackDevMiddleware(compiler, {
+  publicPath: config.output.publicPath
+}));
 
 app.use(express.json());
 app.use(express.static('dist'));
