611c52b5ae4297adfd5525ec5256f28f42eb9f3d
Megan Sullivan <meganesulli@gmail.com>
2019-03-18 23:06:43 -0700

    [Megan][#1] Outlines initial tests for CurrentCommand

---
+++ components/CommandLine/components/CurrentCommand/index.spec.js
@@ -0,0 +1,26 @@
+describe('CurrentCommand', () => {
+  describe('when it has focus', () => {
+    describe('when the user types something', () => {
+      it('updates the text displayed to match user input', () => {
+
+      });
+    });
+    describe('when the user presses enter', () => {
+      it('calls the handleEnter prop', () => {
+
+      });
+    });
+  });
+  describe('when it does not have focus', () => {
+    describe('when the user types something', () => {
+      it('does not update the text displayed', () => {
+
+      });
+    });
+    describe('when the user presses enter', () => {
+      it('does not call the handleEnter prop', () => {
+
+      });
+    });
+  });
+});
